0e630ce9d6230bcb46d8b67a977fe0ff07a07806
Erikguaymas <erikleo97321@gmail.com>
2022-09-05 21:56:02 -0300

    detalles final del desafio

--- Proyecto/proyecto.js
+++ Proyecto/proyecto.js
@@ -1,113 +1,124 @@
-let informe="";
+let informe = "";
 let variacion;
 let dif;
 let opcion;
 let cantidad;
+let i = 0;
 //FUNCIONES
-const variacionPorcentual=(ventaActual,ventaAnterior)=>{return [(ventaActual-ventaAnterior)/ventaAnterior]*100;}
+const variacionPorcentual = (ventaActual, ventaAnterior) => {
+  return [(ventaActual - ventaAnterior) / ventaAnterior] * 100;
+};
 
-const diferencia=(ventaActual,ventaAnterior)=>{return ventaActual-ventaAnterior;}
+const diferencia = (ventaActual, ventaAnterior) => {
+  return ventaActual - ventaAnterior;
+};
 
-function validacion(valor){
-
-    while(valor=="" || isNaN(valor))
-    {
-     if(valor=="") {
-        alert("No ingresó nada");
-        valor=prompt(`Por favor, ingrese correctamente "la cantidad" de camisetas vendidas`);
-        }
-    if(isNaN(valor)){
-        alert("Ingresó un dato no solicitado");
-        valor=prompt(`Por favor, ingrese correctamente "la cantidad" de camisetas vendidas`);
-        }
+function validacion(valor) {
+  while (valor == "" || isNaN(valor)) {
+    if (valor == "") {
+      alert("No ingresó nada");
+      valor = prompt(
+        `Por favor, ingrese correctamente "la cantidad" de camisetas vendidas`
+      );
     }
-return valor;
+    if (isNaN(valor)) {
+      alert("Ingresó un dato no solicitado");
+      valor = prompt(
+        `Por favor, ingrese correctamente "la cantidad" de camisetas vendidas`
+      );
+    }
+  }
+  return valor;
 }
 
-  function calculo(valorEntrada){
-    
-    for(let i=2;i<=meses.length;i++){
-
-        cantidad=prompt(`Ingrese la cantidad vendidas en el mes ${meses[i-1]}`);
-            if((cantidad=="" ) || (isNaN(cantidad))){
-                cantidad=validacion(cantidad);
-            }
-            if(valorEntrada<cantidad)
-            {
-                variacion=variacionPorcentual(cantidad, valorEntrada);
-                dif=diferencia(cantidad, valorEntrada);
-                informe=`El mes de ${meses[i-1]} hubo un aumento de ${variacion}%, se vendio ${dif} mas que el mes anterior  \n`;
-                
-console.log(`venta en el mes ${meses[i-2]}: ${valorEntrada}
-Venta en el mes ${meses[i-1]}: ${cantidad}
-${informe}`)
-            }
-            else if(valorEntrada>cantidad)
-            {
-                variacion=variacionPorcentual(cantidad, valorEntrada);
-                dif=diferencia(cantidad, valorEntrada);
-                informe=`El mes de ${meses[i-1]} hubo una bajada de ${variacion}%, se vendio ${dif} menos que el mes anterior \n`;
-                
-console.log(`Venta en el mes ${meses[i-2]}: ${valorEntrada}
-Venta en el mes ${meses[i-1]}: ${cantidad}
-${informe}`)
-            }
-            else{   
-                informe=`El mes de ${meses[i-1]} no hubo aumento ni bajada, se vendio igual que el mes anterior \n`;
-                
-console.log(`Venta en el mes ${meses[i-2]} :${valorEntrada}
-Venta en el mes ${meses[i-1]} :${cantidad}
-${informe}`)
-                } 
-    valorEntrada=cantidad;
-     }
+function calculo() {
+  for (i = 2; i <= meses.length; i++) {
+    cantidad = Number(
+      prompt(`Ingrese la cantidad vendidas en el mes ${meses[i - 1]}`)
+    );
+    if (cantidad == "" || isNaN(cantidad)) {
+      cantidad = validacion(cantidad);
+    }
+    console.log(vendido);
+    console.log(cantidad);
+    if (cantidad > vendido) {
+      variacion = variacionPorcentual(cantidad, vendido);
+      dif = diferencia(cantidad, vendido);
+      informe = `El mes de ${
+        meses[i - 1]
+      } hubo un aumento de ${variacion}%, se vendio ${dif} mas que el mes anterior  \n`;
+      imprime();
+    } else if (cantidad < vendido) {
+      variacion = variacionPorcentual(cantidad, vendido);
+      dif = diferencia(cantidad, vendido);
+      informe = `El mes de ${
+        meses[i - 1]
+      } hubo una bajada de ${variacion}%, se vendio ${dif} menos que el mes anterior \n`;
+      imprime();
+    } else {
+      informe = `El mes de ${
+        meses[i - 1]
+      } no hubo aumento ni bajada, se vendio igual que el mes anterior \n`;
+      imprime();
+    }
+    vendido = cantidad;
+  }
+}
+function imprime() {
+  console.log(`venta en el mes ${meses[i - 2]}: ${vendido}
+Venta en el mes ${meses[i - 1]}: ${cantidad}
+${informe}`);
 }
 
+
+//PROGRAMA
 //array
-const productos=[
-    {   id:1,
-        nombre:"camiseta",
-        equipo: "Barcelona",
-        stock: 500},
-    {   id:2, 
-        nombre:"short",
-        equipo: "Barcelona",
-        stock:300},
-    {   id:3,
-        nombre:"campera",
-        equipo: "Barcelona",
-        stock: 250},
-    {   id:4,
-        nombre:"botines",
-        marca: "ADIDAS",
-        stock:200}
-]
-const meses=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+const productos = [
+  { id: 1, nombre: "camiseta", equipo: "Barcelona", stock: 1500 },
+  { id: 2, nombre: "short", equipo: "Barcelona", stock: 1200 },
+  { id: 3, nombre: "campera", equipo: "Barcelona", stock: 1000 },
+  { id: 4, nombre: "botines", marca: "ADIDAS", stock: 1300 },
+];
+const meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
 
 alert("A continuación te mostraré en la consola los productos disponibles");
 
-//console.log(productos);
-for (const producto of productos){
-
-    console.log(producto);
+for (const producto of productos) {
+  console.log(producto);
 }
 
-do{ 
-    opcion=Number(prompt(`Elija el id del producto que quisiera evaluar su variacion:`))
-  }
-while(opcion!=1 && opcion!=2 && opcion!=3 && opcion!=4)
+do {
+  opcion = Number(
+    prompt(`Elija el id del producto que quisiera evaluar su variacion:`)
+  );
+} while (opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4);
 
-console.log(`Su stock es de ${productos[opcion-1].stock}`);
+console.log(
+  `Seleccionó el producto "${productos[opcion - 1].nombre.toLocaleUpperCase()}"`
+);
 
-let vendido=prompt(`Ingrese la cantidad vendida en el primer mes del año`);
-        
-if((vendido=="" ) || (isNaN(vendido))){
-    vendido=validacion(vendido);}
+let vendido = Number(
+  prompt(`Ingrese la cantidad vendida en el primer mes del año`)
+);
 
-let porcentual=(vendido*100)/productos[opcion-1].stock;
-console.log(`${productos[opcion-1].nombre.toLocaleUpperCase()}
-            Se vendio el ${porcentual}% del stock en el mes de ${meses[0]}`);
+if (vendido == "" || isNaN(vendido)) {
+  vendido = validacion(vendido);
+}
 
-calculo(vendido);
+let porcentual = (vendido * 100) / productos[opcion - 1].stock;
+console.log(`Se vendio el ${porcentual}% del stock en el mes de ${meses[0]}`);
 
-
+calculo();
