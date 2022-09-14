86b31a83eedfd2ff58e8ab620cf932ee1b4e5ae1
Erikguaymas <erikleo97321@gmail.com>
2022-09-13 22:43:50 -0300

    DOM

--- Proyecto/entrega.js
+++ Proyecto/entrega.js
@@ -1,3 +1,4 @@
+/*
 //DECLARACION
 let arrayDeportivo = [];
 let pregunta;
@@ -91,21 +92,112 @@ function descuento() {
   console.log(`Con tarjeta se va a $${desc}`);
 }
 
-
+*/
 
 //PROGRAMA
+let i = 0;
+
+const nombre = prompt("Ingrese su nombre");
+
+const saludo = document.getElementById("nombre");
+
+const parrafo = document.createElement("h1");
+
+parrafo.innerHTML = `Hola bienvenido ${nombre}, acontinuación te mostraremos los productos disponibles`;
+saludo.appendChild(parrafo);
+
 const deportivos = [
-  { id: 1, nombre: "camiseta", equipo: "Selección Argentina", precio: 15000 },
-  { id: 2, nombre: "camiseta", equipo: "Real Madrid", precio: 17000 },
-  { id: 3, nombre: "campera", equipo: "Boca Juniors", precio: 18000 },
-  { id: 4, nombre: "short", equipo: "River Plate", precio: 6000 },
-  { id: 5, nombre: "camperón", equipo: "Barcelona", precio: 20000 },
-  { id: 6, nombre: "pantalón", equipo: "PSG", precio: 8000 },
+  {
+    id: 1,
+    nombre: "Camiseta",
+    equipo: "Selección Argentina",
+    precio: 18000,
+    imagen: "img/CamisetaSelArg.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Camiseta",
+    equipo: "Real Madrid",
+    precio: 17000,
+    imagen: "img/CamisetaReal.png",
+  },
+  {
+    id: 3,
+    nombre: "Campera",
+    equipo: "Boca Juniors",
+    precio: 18000,
+    imagen: "img/CamperaBOCA.JPG",
+  },
+  {
+    id: 4,
+    nombre: "Short",
+    equipo: "River Plate",
+    precio: 6000,
+    imagen: "img/ShortRIVER.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Camperón",
+    equipo: "Barcelona",
+    precio: 20000,
+    imagen: "img/chaquetaBARSA.jpeg",
+  },
+  {
+    id: 6,
+    nombre: "Pantalón",
+    equipo: "PSG",
+    precio: 8000,
+    imagen: "img/PantalonPSG.jpg",
+  },
 ];
 
-alert(
-  "Hola bienvenido, acontinuación le mostraremos los productos disponibles en la consola"
-);
+let div = document.getElementById("cards");
+console.log(div);
+
+deportivos.forEach((elemento) => {
+  let cont = document.createElement("div");
+  cont.className = "card";
+  cont.style.maxWidth = "400px";
+  cont.innerHTML = `
+<div class="row g-0">
+  <div class="col-md-4">
+    <img src="${elemento.imagen}" class="img-fluid rounded-start" alt="ERROR">
+  </div>
+<div class="col-md-8">
+  <div class="card-body">
+    <h5 class="card-title">${elemento.nombre} ${elemento.equipo}</h5>
+    <p class="card-text">Precio: $${elemento.precio}</p>
+    <button type="button" class="btn btn-dark">Agregar al carrito</button>
+  </div>
+</div>
+</div>
+`;
+  div.appendChild(cont);
+});
+
+let aprobacion = document.getElementById("aprobacion");
+
+aprobacion.innerHTML = `<h3>Selecciona tu experiencia en la tienda</h3>
+<div class="form-check">
+<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+<label class="form-check-label" for="flexRadioDefault1">
+  Horrible
+</label>
+</div>
+<div class="form-check">
+<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+<label class="form-check-label" for="flexRadioDefault2">
+  Regular
+</label>
+</div>
+<div class="form-check">
+<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+<label class="form-check-label" for="flexRadioDefault2">
+  Excelente
+</label>
+</div>`;
+
+/*
 
 for (const elemento of deportivos) {
   console.log(elemento);
@@ -134,3 +226,4 @@ do {
 
 total();
 alert("Gracias por la compra");
+*/
